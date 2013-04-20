72698d39cd3b55c9e04fd12584b74b1f516886a4
lortega <luis.ortega.araneda@gmail.com>
2013-04-20 18:41:51 -0400

    funcionando a medias

--- kml_parser/kmlparser.rb
+++ kml_parser/kmlparser.rb
@@ -6,9 +6,42 @@ xml_data = Net::HTTP.get_response(URI.parse(url)).body
 
 doc = REXML::Document.new(xml_data)
 
-doc.elements.each('kml/Document/Folder/Folder/Folder/Placemark') do |ele|
-  print ele.to_s
+#
+# Los datos que necesitamos
+# - Nombre del evento, con el número
+# - Fecha del evento
+# - Latitud
+# - Longitud
+# - Locacion
+# - Experiencia
+#
+
+registros = []
+
+doc.elements.each('kml/Document/Folder') do |element|
+
+#  if element.get_elements('name').first == "Witnesses"
+#    print "wwww"
+#  end
+
+  #registro = {
+  #  'nombre' => element.get_elements('name').first.to_s
+    #'fecha'  => element.to_s
+    #'latitud' => element.to_s
+    #'longitud' => element.to_s
+    #'locacion' => element.to_s
+    #'experiencia' => element.to_s
+  #}
+
+#  registros << registro
 end
+
+print registros.to_s
+
+
+#doc.elements.each('kml/Document/Folder/Folder/Folder/Placemark') do |ele|
+#  print ele.to_s
+#end
 
 
 #puts doc
