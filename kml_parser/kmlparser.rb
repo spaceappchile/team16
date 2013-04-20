3af0e307279e845a52d55edcee03d88fc032f0eb
lortega <luis.ortega.araneda@gmail.com>
2013-04-20 17:10:07 -0400

    Parselador de KML funcionando

---
+++ kml_parser/kmlparser.rb
@@ -0,0 +1,14 @@
+require 'net/http'
+require 'rexml/document'
+
+url = 'http://www.amsmeteors.org/members/kml/view_event_kml?event_id=895&event_year=2013'
+xml_data = Net::HTTP.get_response(URI.parse(url)).body
+
+doc = REXML::Document.new(xml_data)
+
+doc.elements.each('kml/Document/Folder') do |ele|
+  print ele.to_s
+end
+
+
+#puts doc
