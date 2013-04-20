549685ecec22016eb05641cb1e8ba5904782ce42
Arnaldo Gaspar <arnaldog@gmail.com>
2013-04-20 15:49:44 -0400

    Lets ROCKgit add .

---
+++ wishmaker_backend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WishmasterBackend::Application.config.secret_token = '1807cb6c825fd1e034f2442d571382399921b77769f96a67ac044c8a4d474d769f76f26c5a975741a0c447b41086d99f7172492ec0520a25dc40ebf333630856'
