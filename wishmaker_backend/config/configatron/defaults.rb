c05a9d7bff0e6421db555901cc732492b91ff22e
Javier Giovannini <kombatdechile@gmail.com>
2013-04-20 22:10:01 -0400

    APN ready to notify

---
+++ wishmaker_backend/config/configatron/defaults.rb
@@ -0,0 +1,9 @@
+# Put all your default configatron settings here.
+
+# Example:
+#   configatron.emails.welcome.subject = 'Welcome!'
+#   configatron.emails.sales_reciept.subject = 'Thanks for your order'
+# 
+#   configatron.file.storage = :s3
+configatron.apn.cert = File.join(Rails.root, 'config','apple_push_notification_development.pem')
+
